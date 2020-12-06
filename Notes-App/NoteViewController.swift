cc9ae300c1c328fda44aa7a83d719a070a73adfe
niles87 <nbing19@gmail.com>
2020-12-06 14:08:56 -0800

    notes view controller

--- Notes-App/NoteViewController.swift
+++ Notes-App/NoteViewController.swift
@@ -5,4 +5,23 @@
 //  Created by Niles Bingham on 12/6/20.
 //
 
-import Foundation
+import UIKit
+
+class NoteViewController: UIViewController {
+    var note: Note!
+    
+    @IBOutlet var textView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        textView.text = note.contents
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        note.contents = textView.text
+        NoteManager.main.save(note: note)
+    }
+}
