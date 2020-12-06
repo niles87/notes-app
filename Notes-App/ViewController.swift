c4fc5c8c1df64bc95677c65479b3805ef9fc461e
niles87 <nbing19@gmail.com>
2020-12-06 11:41:21 -0800

    Initial Commit

---
+++ Notes-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notes-App
+//
+//  Created by Niles Bingham on 12/6/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
